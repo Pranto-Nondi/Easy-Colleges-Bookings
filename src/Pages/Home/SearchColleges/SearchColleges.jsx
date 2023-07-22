5cceb39f05f2e4fdf4ffe3d8e2260552be0ef718
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-07-23 02:11:17 +0600

    progress

--- src/Pages/Home/SearchColleges/SearchColleges.jsx
+++ src/Pages/Home/SearchColleges/SearchColleges.jsx
@@ -28,7 +28,7 @@ const SearchColleges = () => {
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1513077202514-c511b41bd4c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZWR1Y2F0aW9uJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80')" }}>
       <div className="w-full max-w-5xl  rounded-lg shadow-md px-4 py-6 md:flex ">
         {/* Left Side - Search Functionality */}
-        <div className="md:w-1/2 md:mr-4 ">
+        <div className=" md:w-1/2 md:mr-4 ">
           <input
             type="text"
             value={searchTerm}
@@ -50,23 +50,22 @@ const SearchColleges = () => {
           {searchResults.length > 0 ? (
             <div>
               <h2 className="text-lg font-semibold mb-2">Search Results:</h2>
-              <div className="grid gap-4">
+              <div className="grid md:grid-cols-2 gap-4">
                 {searchResults.map((college) => (
                   <div
                     key={college._id}
-                    className="p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+                    className=" h-auto  md:w-60 p-4 bg-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
                   >
-                    <h3 className="text-lg font-semibold">{college.name}</h3>
-                    <p className="text-gray-600">{college.description}</p>
+                    <img src="https://images.unsplash.com/20/cambridge.JPG?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aGFydmFyZCUyMHVuaXZlcnNpdHl8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+                    <p className="text-gray-600 font-semibold text-lg">{college.name}</p>
+                    <p className="text-gray-600">{college.details}</p>
+                     <button className='btn btn-sm'>Details</button>
                   </div>
                 ))}
               </div>
             </div>
-          ) : (
-            searchTerm !== '' && (
-              <p className="mt-4 text-center text-gray-600">No results found for "{searchTerm}".</p>
-            )
-          )}
+          ) : ""}
+          
         </div>
       </div>
     </div>
