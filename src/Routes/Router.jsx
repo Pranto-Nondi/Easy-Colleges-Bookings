36674036156708460b9c3159b6bd20023baf8c08
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-07-22 13:18:57 +0600

    progress

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+        // errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <Home/>,
+
+            },
+            // {
+            //     path: 'login',
+            //     element: <Login />
+            // },
+            
+        ]
+    },
+   
+
+]);
+export default router
